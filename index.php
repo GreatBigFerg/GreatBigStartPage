a59b68dfe2134778c1fbdb550f5c665c3fe8f434
Bryce Ferguson <34407581+GreatBigFerg@users.noreply.github.com>
2019-07-18 17:53:59 -0400

    commit

--- index.php
+++ index.php
@@ -18,27 +18,23 @@ session_start();
 	</div>
 	<!-- Tiles -->
 	<div id="tile-container">
+		
 		<!--  -->
-		<a id="github" href="https://github.com/GreatBigFerg" class="tile">
+		<a id="umuc" href="https://learn.umuc.edu/d2l/home" class="tile">
 			<div class="tile-content">
-				<b>Gi</b>
-				Github
+				<b>U</b>
+				UMUC
 			</div>
 		</a>
 		<!--  -->
-		<a id="gmail" href="https://mail.google.com/mail/ca/u/0/#inbox" class="tile">
+		<a id="replit" href="https://repl.it/repls" class="tile">
 			<div class="tile-content">
-				<b>Gm</b>
-				Gmail
+				<b>Re</b>
+				Repl.it
 			</div>
 		</a>
-		<!--  -->
-		<a id="amazon" href="https://www.amazon.com/" class="tile">
-			<div class="tile-content">
-				<b>Am</b>
-				Amazon
-			</div>
-		</a>
+
+		
 		<!--  -->
 		<a id="youtube" href="https://www.youtube.com/" class="tile">
 			<div class="tile-content">
@@ -58,6 +54,46 @@ session_start();
 			<div class="tile-content">
 				<b>Ca</b>
 				Calendar
+			</div>
+		</a>
+		<!--  -->
+		<a id="messages" href="https://messages.google.com/web/authentication?redirected=true" class="tile">
+			<div class="tile-content">
+				<b>M</b>
+				Messages
+			</div>
+		</a>
+		
+		
+		<!--  -->
+		<a id="spotify" href="https://www.spotify.com/us/" class="tile">
+			<div class="tile-content">
+				<b>Sp</b>
+				Spotify
+			</div>
+		</a>
+		<!--  -->
+		<a id="steam" href="https://store.steampowered.com/" class="tile">
+			<div class="tile-content">
+				<b>St</b>
+				Steam
+			</div>
+		</a>
+		<!--  -->
+		<a id="xbox" href="https://www.xbox.com/en-US/" class="tile">
+			<div class="tile-content">
+				<b>X</b>
+				XBOX
+			</div>
+		</a>
+
+
+		
+		<!--  -->
+		<a id="reddit" href="https://www.reddit.com/" class="tile">
+			<div class="tile-content">
+				<b>R</b>
+				Reddit
 			</div>
 		</a>
 		<!--  -->
@@ -82,52 +118,124 @@ session_start();
 			</div>
 		</a>
 		<!--  -->
+		<a id="pinterest" href="https://www.pinterest.com/" class="tile">
+			<div class="tile-content">
+				<b>Pi</b>
+				Pinterest
+			</div>
+		</a>
+
+		
+		
+		<!--  -->
+		<a id="speedtest" href="https://www.speedtest.net/" class="tile">
+			<div class="tile-content">
+				<b>Sp</b>
+				SpeedTest
+			</div>
+		</a>
+
+
+		
+		<!--  -->
+		<a id="pulseway" href="https://my.pulseway.com/" class="tile">
+			<div class="tile-content">
+				<b>Pu</b>
+				PulseWay
+			</div>
+		</a>
+		
+
+		<!--  -->
+		<a id="gmail" href="https://mail.google.com/mail/ca/u/0/#inbox" class="tile">
+			<div class="tile-content">
+				<b>Gm</b>
+				Gmail
+			</div>
+		</a>
+		<!--  -->
+		<a id="icloud" href="https://www.icloud.com/" class="tile">
+			<div class="tile-content">
+				<b>iC</b>
+				iCloud
+			</div>
+		</a>
+		<!--  -->
+		<a id="aol" href="https://login.aol.com" class="tile">
+			<div class="tile-content">
+				<b>Ao</b>
+				AOL Mail
+			</div>
+		</a>
+		<!--  -->
+		<a id="yahoo" href="https://login.yahoo.com" class="tile">
+			<div class="tile-content">
+				<b>Ya</b>
+				Yahoo Mail
+			</div>
+		</a>
+
+
+		<!--  -->
+		<a id="newegg" href="https://www.newegg.com/" class="tile">
+			<div class="tile-content">
+				<b>Ne</b>
+				Newegg
+			</div>
+		</a>
+		<!--  -->
+		<a id="amazon" href="https://www.amazon.com/" class="tile">
+			<div class="tile-content">
+				<b>Am</b>
+				Amazon
+			</div>
+		</a>
+
+
+		<!--  -->
+		<a id="primevideo" href="https://www.amazon.com/Amazon-Video" class="tile">
+			<div class="tile-content">
+				<b>Pr</b>
+				PrimeVideo
+			</div>
+		</a>
+
+
+		<!--  -->
 		<a id="plex" href="http://127.0.0.1:32400/web/index.html" class="tile">
 			<div class="tile-content">
 				<b>Pl</b>
 				Plex
 			</div>
 		</a>
+
+
 		<!--  -->
-		<a id="reddit" href="https://www.reddit.com/" class="tile">
+		<a id="github" href="https://github.com/GreatBigFerg" class="tile">
 			<div class="tile-content">
-				<b>R</b>
-				Reddit
+				<b>Gi</b>
+				Github
 			</div>
 		</a>
 		<!--  -->
-		<a id="slack" href="" class="tile">
+		<a id="cpanel" href="https://p3plcpnl0989.prod.phx3.secureserver.net:2083/cpsess5554989565/frontend/gl_paper_lantern/index.html" class="tile">
 			<div class="tile-content">
-				<b>Sl</b>
-				Slack
+				<b>cP</b>
+				cPanel
 			</div>
 		</a>
 		<!--  -->
-		<a id="spotify" href="https://www.spotify.com/us/" class="tile">
+		<a id="godaddy" href="https://sso.godaddy.com/login?realm=idp&app=account&port=443&path=/products/#/" class="tile">
 			<div class="tile-content">
-				<b>Sp</b>
-				Spotify
+				<b>Go</b>
+				GoDaddy
 			</div>
 		</a>
 		<!--  -->
-		<a id="steam" href="https://store.steampowered.com/" class="tile">
+		<a id="googledevelopers" href="https://developers.google.com/" class="tile">
 			<div class="tile-content">
-				<b>St</b>
-				Steam
-			</div>
-		</a>
-		<!--  -->
-		<a id="xbox" href="https://www.xbox.com/en-US/" class="tile">
-			<div class="tile-content">
-				<b>X</b>
-				XBOX
-			</div>
-		</a>
-		<!--  -->
-		<a id="pinterest" href="https://www.pinterest.com/" class="tile">
-			<div class="tile-content">
-				<b>Pi</b>
-				Pinterest
+				<b>G</b>
+				Google Developers
 			</div>
 		</a>
 		<!-- 
