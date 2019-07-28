4872ec6fc450f23fd1fb7aad33d96a9de9f10ba7
Bryce Ferguson <bryceferguson.css@gmail.com>
2019-07-28 10:10:57 -0400

    add some style

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ session_start();
 <head>
 <title>GreatBigStartPage</title>
 <link href="../css/style.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
 <!-- &nbsp; -->    <!--  -->
@@ -21,14 +23,14 @@ session_start();
 		
 		<!--  -->
 		<a id="umuc" href="https://learn.umuc.edu/d2l/home" target="_blank" class="tile">
-			<div class="tile-content">
+			<div class="tile-content no-icon">
 				<b>U</b>
 				UMUC
 			</div>
 		</a>
 		<!--  -->
 		<a id="replit" href="https://repl.it/repls" target="_blank" class="tile">
-			<div class="tile-content">
+			<div class="tile-content no-icon">
 				<b>Re</b>
 				Repl.it
 			</div>
@@ -48,6 +50,9 @@ session_start();
 				<b>Dr</b>
 				Drive
 			</div>
+			<div class="tile-icon">
+				<i class='fab fa-google-drive'></i>
+			</div>
 		</a>
 		<!--  -->
 		<a id="calendar" href="https://calendar.google.com/calendar/r" target="_blank" class="tile">
@@ -55,12 +60,18 @@ session_start();
 				<b>Ca</b>
 				Calendar
 			</div>
+			<div class="tile-icon">
+				<i class='far fa-calendar-alt'></i>
+			</div>			
 		</a>		
 		<!--  -->
 		<a id="messages" href="https://messages.google.com/web/authentication?redirected=true" target="_blank" class="tile">
 			<div class="tile-content">
 				<b>M</b>
 				Messages
+			</div>
+			<div class="tile-icon">
+				<i class='material-icons' style="font-size:inherit">message</i>
 			</div>
 		</a>
 
@@ -74,7 +85,7 @@ session_start();
 
 		<!--  -->
 		<a id="gmail" href="https://mail.google.com/mail/ca/u/0/#inbox" target="_blank" class="tile">
-			<div class="tile-content">
+			<div class="tile-content no-icon">
 				<b>Gm</b>
 				Gmail
 			</div>
@@ -82,7 +93,7 @@ session_start();
 		
 		<!--  -->
 		<a id="aol" href="https://login.aol.com" target="_blank" class="tile">
-			<div class="tile-content">
+			<div class="tile-content no-icon">
 				<b>Ao</b>
 				AOL Mail
 			</div>
@@ -93,12 +104,18 @@ session_start();
 				<b>Ya</b>
 				Yahoo Mail
 			</div>
+			<div class="tile-icon">
+				<i class='fab fa-yahoo'></i>
+			</div>
 		</a>
 		<!--  -->
 		<a id="icloud" href="https://www.icloud.com/" target="_blank" class="tile">
 			<div class="tile-content">
 				<b>iC</b>
 				iCloud
+			</div>
+			<div class="tile-icon">
+				<i class='fab fa-apple'></i>
 			</div>
 		</a>
 		
@@ -116,6 +133,9 @@ session_start();
 				<b>Gi</b>
 				Github
 			</div>
+			<div class="tile-icon">
+				<i class='fab fa-github'></i>
+			</div>
 		</a>
 		<!--  -->
 		<a id="cpanel" href="https://p3plcpnl0989.prod.phx3.secureserver.net:2083/cpsess5554989565/frontend/gl_paper_lantern/index.html" target="_blank" class="tile">
@@ -123,31 +143,34 @@ session_start();
 				<b>cP</b>
 				cPanel
 			</div>
+			<div class="tile-icon">
+				<i class='fab fa-cpanel'></i>
+			</div>
 		</a>
 		<!--  -->
 		<a id="godaddy" href="https://sso.godaddy.com/login?realm=idp&app=account&port=443&path=/products/#/" target="_blank" class="tile">
-			<div class="tile-content">
+			<div class="tile-content no-icon">
 				<b>Go</b>
 				GoDaddy
 			</div>
 		</a>
 		<!--  -->
 		<a id="pulseway" href="https://my.pulseway.com/" target="_blank" class="tile">
-			<div class="tile-content">
+			<div class="tile-content no-icon">
 				<b>Pu</b>
 				PulseWay
 			</div>
 		</a>
 		<!--  -->
 		<a id="plex" href="http://127.0.0.1:32400/web/index.html" target="_blank" class="tile">
-			<div class="tile-content">
+			<div class="tile-content no-icon">
 				<b>Pl</b>
 				Plex
 			</div>
 		</a>
 		<!--  -->
 		<a id="speedtest" href="https://www.speedtest.net/" target="_blank" class="tile">
-			<div class="tile-content">
+			<div class="tile-content no-icon">
 				<b>Sp</b>
 				SpeedTest
 			</div>
@@ -160,135 +183,181 @@ session_start();
 				
 			</div>
 		</a>
-
+		
 		<!--  -->
-		<a id="reddit" href="https://www.reddit.com/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>R</b>
-				Reddit
+		<div class="dropdown-button tile" onclick="showItem('social')">
+			<div class="tile-content">	
+				Social
 			</div>
-		</a>
-		<!--  -->
-		<a id="twitter" href="https://twitter.com/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>T</b>
-				Twitter
-			</div>
-		</a>
-		<!--  -->
-		<a id="facebook" href="https://www.facebook.com/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>Fa</b>
-				Facebook
-			</div>
-		</a>
-		<!--  -->
-		<a id="linkedin" href="https://www.linkedin.com/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>Li</b>
-				LinkedIn
-			</div>
-		</a>
-		<!--  -->
-		<a id="pinterest" href="https://www.pinterest.com/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>Pi</b>
-				Pinterest
-			</div>
-		</a>
-
-		<!--  -->
-		<a id="" href="" class="tile spacer">
-			<div class="tile-content">
-				<b></b>
-				
-			</div>
-		</a>
-
-		<!--  -->
-		<a id="youtube" href="https://www.youtube.com/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>Y</b>
-				Youtube
-			</div>
-		</a>
-		<!--  -->
-		<a id="spotify" href="https://www.spotify.com/us/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>Sp</b>
-				Spotify
-			</div>
-		</a>
-		<!--  -->
-		<a id="steam" href="https://store.steampowered.com/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>St</b>
-				Steam
-			</div>
-		</a>
-		<!--  -->
-		<a id="xbox" href="https://www.xbox.com/en-US/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>X</b>
-				XBOX
-			</div>
-		</a>
-		<!--  -->
-		<a id="primevideo" href="https://www.amazon.com/Amazon-Video" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>Pr</b>
-				PrimeVideo
-			</div>
-		</a>
-
-		<!--  -->
-		<a id="" href="" class="tile spacer">
-			<div class="tile-content">
-				<b></b>
-				
-			</div>
-		</a>
-
-
-		<!--  -->
-		<div class="dropdown-button" onclick="showItem('shopping')">
-			shopping
 		</div>
-		<div class="dropdown-container">			
-			<div id="shopping" class="dropdown-content">
+		<div id="social" class="dropdown-container">			
+			<div class="dropdown-content">
+				<!--  -->
+				<a id="reddit" href="https://www.reddit.com/" target="_blank" class="tile">
+					<div class="tile-content">
+						<b>R</b>
+						Reddit
+					</div>
+					<div class="tile-icon">
+						<i class='fab fa-reddit'></i>
+					</div>
+				</a>
+				<!--  -->
+				<a id="twitter" href="https://twitter.com/" target="_blank" class="tile">
+					<div class="tile-content">
+						<b>T</b>
+						Twitter
+					</div>
+					<div class="tile-icon">
+						<i class='fab fa-twitter'></i>
+					</div>
+				</a>
+				<!--  -->
+				<a id="facebook" href="https://www.facebook.com/" target="_blank" class="tile">
+					<div class="tile-content">
+						<b>Fa</b>
+						Facebook
+					</div>
+					<div class="tile-icon">
+						<i class='fab fa-facebook'></i>
+					</div>
+				</a>
+				<!--  -->
+				<a id="linkedin" href="https://www.linkedin.com/" target="_blank" class="tile">
+					<div class="tile-content">
+						<b>Li</b>
+						LinkedIn
+					</div>
+					<div class="tile-icon">
+						<i class='fab fa-linkedin'></i>
+					</div>
+				</a>
+				<!--  -->
+				<a id="pinterest" href="https://www.pinterest.com/" target="_blank" class="tile">
+					<div class="tile-content">
+						<b>Pi</b>
+						Pinterest
+					</div>
+					<div class="tile-icon">
+						<i class='fab fa-pinterest'></i>
+					</div>
+				</a>
+			</div>
+		</div>
+
+		<!--  -->
+		<a id="" href="" class="tile spacer">
+			<div class="tile-content">
+				<b></b>
+				
+			</div>
+		</a>
+
+		<!--  -->
+		<div class="dropdown-button tile" onclick="showItem('entertainment')">
+			<div class="tile-content">		
+				Entertainment
+			</div>
+		</div>
+		<div id="entertainment" class="dropdown-container">			
+			<div class="dropdown-content">
+				<!--  -->
+				<a id="youtube" href="https://www.youtube.com/" target="_blank" class="tile">
+					<div class="tile-content">
+						<b>Y</b>
+						Youtube
+					</div>
+					<div class="tile-icon">
+						<i class='fab fa-youtube'></i>
+					</div>
+				</a>
+				<!--  -->
+				<a id="spotify" href="https://www.spotify.com/us/" target="_blank" class="tile">
+					<div class="tile-content">
+						<b>Sp</b>
+						Spotify
+					</div>
+					<div class="tile-icon">
+						<i class='fab fa-spotify'></i>
+					</div>
+				</a>
+				<!--  -->
+				<a id="steam" href="https://store.steampowered.com/" target="_blank" class="tile">
+					<div class="tile-content">
+						<b>St</b>
+						Steam
+					</div>
+					<div class="tile-icon">
+						<i class='fab fa-steam'></i>
+					</div>
+				</a>
+				<!--  -->
+				<a id="xbox" href="https://www.xbox.com/en-US/" target="_blank" class="tile">
+					<div class="tile-content">
+						<b>X</b>
+						XBOX
+					</div>
+					<div class="tile-icon">
+						<i class='fab fa-xbox'></i>
+					</div>
+				</a>
+				<!--  -->
+				<a id="netflix" href="https://www.netflix.com/browse" target="_blank" class="tile">
+					<div class="tile-content no-icon">
+						<b>Ne</b>
+						Netflix
+					</div>
+				</a>
+				<!--  -->
+				<a id="primevideo" href="https://www.amazon.com/Amazon-Video" target="_blank" class="tile">
+					<div class="tile-content no-icon">
+						<b>Pr</b>
+						PrimeVideo
+					</div>
+				</a>
+			</div>
+		</div>
+
+		<!--  -->
+		<a id="" href="" class="tile spacer">
+			<div class="tile-content">
+				<b></b>
+				
+			</div>
+		</a>
+
+		<!--  -->
+		<div class="dropdown-button tile" onclick="showItem('shopping')">
+			<div class="tile-content">	
+				Shopping
+			</div>
+		</div>
+		<div id="shopping" class="dropdown-container">			
+			<div class="dropdown-content">
 				<!--  -->
 				<a id="amazon" href="https://www.amazon.com/" target="_blank" class="tile">
 					<div class="tile-content">
 						<b>Am</b>
 						Amazon
 					</div>
+					<div class="tile-icon">
+						<i class='fab fa-amazon'></i>
+					</div>
 				</a>
 				<!--  -->
 				<a id="newegg" href="https://www.newegg.com/" target="_blank" class="tile">
-					<div class="tile-content">
+					<div class="tile-content no-icon">
 						<b>Ne</b>
 						Newegg
 					</div>
 				</a>
 			</div>
 		</div>
-
-		<!--  
-		<a id="amazon" href="https://www.amazon.com/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>Am</b>
-				Amazon
-			</div>
-		</a>
-		
-		<a id="newegg" href="https://www.newegg.com/" target="_blank" class="tile">
-			<div class="tile-content">
-				<b>Ne</b>
-				Newegg
-			</div>
-		</a>   -->
 		
 		<!-- 
+		<div class="tile-icon">
+			<i class=''></i>
+		</div>
 		<a id="" href="" target="_blank" class="tile">
 			<div class="tile-content">
 				<b></b>
@@ -299,20 +368,6 @@ session_start();
 	</div>
 </div>
 <script>
-	function showmore(id) {
-		var x = document.getElementById(id);
-		if (x.className.indexOf("hide") == -1) {
-			x.className += " hide";
-			x.style.display = "block";
-		} 
-        else if (x.style.display == "none") {
-            x.style.display = x.style.display = "block !important";
-        }
-        else {
-			x.className = x.className.replace(" hide", "");
-			x.style.display = "none !important";
-		}
-	}
 	function showItem(id) {
         item = document.getElementById(id);
         if (item.style.display == 'none') {
